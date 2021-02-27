112c46baf5e32eaed8670cd7970579b1ad77424c
Ashab <ashabuddin54321@gmail.com>
2021-02-27 05:59:15 -0800

    ultra nation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Country from './Component/Country/Country';
+import Cart from './Component/Cart/Cart';
+
 
 function App() {
-  return (
+  const[countries, setCountries] =useState([]);
+  const [cart, setCart] = useState([]);
+  useEffect(()=>{
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => {
+      setCountries(data)
+     data.map(country => country.name)
+    })
+  }, [])
+  const handleAddCountry =(country)=>{
+    const newCart = [...cart, country]
+    setCart(newCart)
+  }
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Country Load:{countries.length}</h1>
+      <h3>Country Added:{cart.length}</h3>
+      <Cart cart={cart}></Cart>
+      <ul>
+         {
+          countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country> )}
+      </ul>
     </div>
   );
 }
